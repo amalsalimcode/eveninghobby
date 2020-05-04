c907b608fb7778f51f9d6fdecae5cf67e2f5fd6b
Amal Salim <amalsalim@Amals-MacBook-Pro.local>
2020-05-04 16:03:11 -0700

    statement graph

--- react-native-first/components/StmtGraph.js
+++ react-native-first/components/StmtGraph.js
@@ -1,15 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet } from 'react-native'
-import { Input } from 'react-native-elements'
+import React, { useRef } from "react";
+import { Animated, Text, View, StyleSheet, Button } from "react-native";
 
+const StmtGraph = props => {
+  // fadeAnim will be used as the value for opacity. Initial Value: 0
+  const fadeAnim = useRef(new Animated.Value(0)).current;
 
-const StmtGraph= props => {
-    return (
-        <View>
-            <Text>Hello</Text>
-        </View>
-    )
+  const fadeIn = () => {
+    // Will change fadeAnim value to 1 in 5 seconds
+    Animated.timing(fadeAnim, {
+      toValue: 200,
+      duration: 1000
+    }).start();
+  };
 
+  const fadeOut = () => {
+    // Will change fadeAnim value to 0 in 5 seconds
+    Animated.timing(fadeAnim, {
+      toValue: 0,
+      duration: 1000
+    }).start();
+  };
+
+  return (
+    <View>
+      <Animated.View style={[styles.fadingContainer, {width: fadeAnim,}]}>
+      </Animated.View>
+
+      <View>
+        <Button title="Fade In" onPress={fadeIn} />
+        <Button title="Fade Out" onPress={fadeOut} />
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  fadingContainer: {
+    paddingVertical: 50,
+    height: 10,
+    backgroundColor: "powderblue",
+    opacity: 1 // Bind opacity to animated value
+  },
+});
 
 export default StmtGraph
