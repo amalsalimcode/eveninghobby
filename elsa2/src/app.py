75a8e171c3a8690f221a469d6236280e8c0189d5
amal salim <amal.salim@gmail.com>
2020-10-27 17:52:48 -0700

    something working

--- elsa2/src/app.py
+++ elsa2/src/app.py
@@ -1,84 +1,86 @@
-import db
-
 from flask import Flask, request, jsonify, render_template
-from werkzeug.security import generate_password_hash, check_password_hash
-from forms.forms import RegistrationForm, LoginForm, ResetPasswordForm
-from common import gen_jwt_token, verify_user
-from flask_wtf.csrf import CSRFProtect
-
+from flask_socketio import SocketIO, emit
+import _thread
+import db
 import json
+from flask_cors import CORS, cross_origin
+
+from common import encode_auth_token
+
+from common import is_event_type_unknown, check_auth
 
 server = Flask(__name__)
-csrf = CSRFProtect(server)
-
-@server.route("/")
-def user_list():
-    return json.dumps(db.get_users_all())
+socketio = SocketIO(server)
+CORS(server)
 
 
-@server.route("/register", methods=['POST', 'GET'])
-def register():
-    f = RegistrationForm(request.form)
-    if request.method == "POST" and f.validate_on_submit():
-        return register_user(f.username.data, f.password.data)
-    else:
-        return render_template('register.html', form=f)
+values = {
+    # 'slider1': 25,
+    # 'slider2': 0,
+    'token': 'test'
+}
+
+# have imei be sent as get request, and then return html with token
+# the connect from the html should send in the token. only then respond
+# then talk via sockets
+
+@server.route("/login", methods=['GET'])
+def index():
+    return render_template('index.html', **values)
 
 
-def register_user(username, password):
-    """helper function to register a user"""
-    if db.user_exists(username):
-        return "User already exists", 409
+@server.route("/", methods=['POST', 'GET'])
+def main():
 
-    hashed_pass = generate_password_hash(password)
-    db.create_user(username, hashed_pass)
-    return "User Created", 200
-
-
-@server.route('/login', methods=['POST', 'GET'])
-def login():
-    f = LoginForm(request.form)
-    if request.method == "POST" and f.validate_on_submit():
-        return login_user(f.username.data, f.password.data)
-    else:
-        return render_template('login.html', form=f)
-
-
-def login_user(username, password):
-    """helper function to login a user"""
-    if verify_user(username, password):
-        token = gen_jwt_token()
-        return jsonify({'token': token}), 200
-    else:
-        return jsonify({'error': 'Incorrect credentials or invalid user'}), 401
-
-
-@server.route('/resetpassword', methods=['GET', 'POST'])
-def resetpassword():
-    f = ResetPasswordForm(request.form)
     if request.method == "POST":
-        username, o_passwd, n_passwd = \
-            f.username.data, f.old_password.data, f.new_password.data
-        return reset_passwd(username, o_passwd, n_passwd)
-    else:
-        return render_template('resetpassword.html', form=f)
+        post_data = json.loads(request.data.decode('ascii'))
+
+        type = post_data.get("type", "").lower()
+        if type == "login":
+            imei = post_data.get("imei")
+
+            token = encode_auth_token(imei)
+            return render_template("index.html", **token)
+
+        resp, status = check_auth(request)
+        if status != 200:
+            return resp, status
+
+        imei = resp
+
+        if type == "alarm":
+            alarm_type = post_data.get("alarm_type")
+            if is_event_type_unknown(alarm_type):
+                return jsonify({"error": "unknown alarm type"}), 400
+
+            al_id = db.create_alarm(imei, alarm_type, post_data.get("alarm_time", ""),
+                            post_data.get("latitude", ""), post_data.get("longitude"))
+
+            for file_name in post_data.get("file_list"):
+                x = db.create_file(al_id, file_name)
+                print("here is the fileid", x)
+
+            return 'Done', 200
+
+        elif type == "location":
+            db.create_location(imei, post_data.get("time"), post_data.get("lat"), post_data.get("long"))
+            return 'Done', 200
 
 
-def reset_passwd(username, o_passwd, n_passwd):
-    """helper function to reset password"""
-    if not verify_user(username, o_passwd):
-        return jsonify({'error': 'User or password are incorrect'}), 401
+@socketio.on('connect')
+def test_connect():
+    print("ok we have a request", request.args.get('token'))
+    emit('after connect',  {'data': 'Lets dance'})
 
-    hashed_pass = generate_password_hash(n_passwd)
-    db.update_credentials(username, hashed_pass)
 
-    return jsonify({'ok': 'password changed'}), 200
+@socketio.on('location update')
+def value_changed(message):
+    values[message['who']] = message['data']
+    emit('update value', message, broadcast=True)
 
 
 if __name__ == "__main__":
-    db.create_credentials_table()
     server.config['SECRET_KEY'] = 'ELSA#@#SECRETKEY$%@999'
-
-    csrf.init_app(server)
-    server.run(host='0.0.0.0')
-
+    db.init_table()
+    _thread.start_new_thread(lambda: socketio.run(server), ())
+    server.run(port=5005)
--- elsa2/src/db.py
+++ elsa2/src/db.py
@@ -1,4 +1,5 @@
 import sqlite3 as sql
+import pymongo
 
 def get_db(read=False):
     con = sql.connect("database.db")
@@ -6,28 +7,54 @@ def get_db(read=False):
         con.row_factory = sql.Row
     return con.cursor(), con
 
+def get_mongo_db(col):
+    m_client = pymongo.MongoClient("mongodb://localhost:27017/")
+    m_db = m_client["mydatabase"]
+    return m_db[col]
+
+
+def create_location(imei, l_time, l_lat, l_long):
+    mycol = get_mongo_db(imei)
+    mycol.insert_one({"time": l_time, "lat": l_lat, "long": l_long})
+
+
 def init_table():
-    cur, con = get_db(True)
-    con.execute('CREATE TABLE IF NOT EXISTS alarm '
+    cur, con = get_db()
+    # con.execute('DROP TABLE alarm')
+    # con.execute('DROP TABLE video_file')
+
+    con.execute('CREATE TABLE if not exists alarm '
                 '(type varchar(100),'
+                'imei TEXT,'
                 'time DATETIME,'
                 'lat TEXT,'
                 'long TEXT)')
 
-    con.execute('CREATE TABLE IF NOT EXISTS video_file'
-                '(id integer PRIMARY KEY NOT NULL,'
-                'file_name VARCHAR(100),'
+    con.execute('CREATE TABLE if not exists video_file'
+                '(file_name VARCHAR(100),'
                 'alarm_id INTEGER,'
-                'location varchar(255),'
                 'FOREIGN KEY (alarm_id) REFERENCES alarm(id))')
 
+    con.commit()
     con.close()
 
+def create_alarm(imei, al_type, al_time, lat, long):
+    cur, con = get_db()
+    con.execute('INSERT INTO alarm (imei, type, time, lat, long) VALUES (?, ?, ?, ?, ?)',
+                [imei, al_type, al_time, lat, long])
+    row_id = con.execute('select last_insert_rowid()').fetchone()[0]
+    con.commit()
+    con.close()
+    return row_id
 
-def create_alarm():
-    cur, con = get_db(True)
-    con.execute('INSERT INTO alarm ()')
-
+def create_file(alarm_id, file_name):
+    cur, con = get_db()
+    con.execute('INSERT INTO video_file (alarm_id, file_name) VALUES (?, ?)',
+                [alarm_id, file_name])
+    row_id = con.execute('select last_insert_rowid()').fetchone()[0]
+    con.commit()
+    con.close()
+    return row_id
 
 def get_users_all():
     cur, con = get_db(True)
