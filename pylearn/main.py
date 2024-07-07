2a43e504cabf90adb6e80cd800bf420d3d0e0c0c
amal salim <amal.salim@gmail.com>
2024-07-07 09:46:46 -0700

    main

--- pylearn/main.py
+++ pylearn/main.py
@@ -18,10 +18,14 @@ import requests
 url = 'https://jsonplaceholder.typicode.com/posts'
 
 r = requests.get(url)
-# print(r.status_code, r.json(), r.content)
+print(r.status_code, r.json(), r.content)
 
-r = requests.post(url, json={'what': "happend"})
-# print(r.status_code, r.json(), r.content)
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer YOUR_ACCESS_TOKEN"
+}
+r = requests.post(url, json={'what': "happend"}, headers=headers)
+print(r.status_code, r.json(), r.content)
 
 # how to return data in http?
 
@@ -101,7 +105,7 @@ print(arr)
 #################
 
 # pick a pivot (in value)
-# two ptrs: j holds position of everything less. i iterates array
+# two ptrs, slo, fast: j holds position of everything less. i iterates array
 # anything less than pivot found, put in j position, increment j
 
 def quick_sort(arr, lo, hi):
@@ -125,6 +129,63 @@ def quick_sort(arr, lo, hi):
 arr = [5,3,12,51,6,7,13,8]
 quick_sort(arr, 0, len(arr))
 print(arr)
+
+
+##################
+## binary search #
+##################
+
+# use <= when looking for exact match
+# use < when looking for closest match
+
+def bin_search(arr, item):
+
+	l, r = 0, len(arr)-1
+	ans = -1
+
+	# equal to because case mid == l == r
+	while l <= r:
+		mid = (l + r) // 2
+
+		if arr[mid] == item:
+			# return idx
+			return mid
+
+		elif arr[mid] > item:
+			r = mid - 1
+
+		else:
+			# case len(arr) == 2, avoid redundant 0
+			l = mid+1
+
+	return ans
+
+print(bin_search([1,3,4,7,23,25], 4))
+
+
+#################
+##### SORT ######
+#################
+
+print(sorted([1, 2, 3, 4], key=lambda x: -x))
+print(sorted([(1,2), (1,3), (5,6), (1,1)], key=lambda x: (x[0], x[1])))
+
+def my_srt(x, y):
+	if x == 55: return -1
+	if y == 55: return 1
+	if x < y: return -1
+	if y < x: return 1
+	if x == y: return 0
+
+print(sorted([1, 2, 3, 4, 55, 55, 22], key=functools.cmp_to_key(my_srt)))
+
+arr.sort()
+arr.sort(reverse=True)
+
+from operator import itemgetter, attrgetter
+arr.sort(key=itemgetter(1))
+arr.sort(key=attrgetter('age'))
+
 
 #################
 ### ITERTOOLS ###
@@ -160,13 +221,13 @@ list(zip([1, 2, 3], [4, 5, 6]))
 
 arr = [1, 2, 3]
 
-[1, 2, 3, 3, 3, 5].count(4)
-
 len(arr)
 
 arr[1:-1]
 
+[1, 2, 3, 3, 3, 5].count(4)
 arr.index(2)
+arr.insert(2, 344)
 
 arr = arr[:]
 
@@ -178,37 +239,11 @@ arr.pop(0)
 
 splice = arr[3:5]
 
-arr.insert(2, 344)
-
 arr[-3:]
 
 set(arr)
 
 ''.join(['a', 'b'])
-
-#################
-##### SORT ######
-#################
-
-print(sorted([1, 2, 3, 4], key=lambda x: -x))
-print(sorted([(1,2), (1,3), (5,6), (1,1)], key=lambda x: (x[0], x[1])))
-
-def my_srt(x, y):
-	if x == 55: return -1
-	if y == 55: return 1
-	if x < y: return -1
-	if y < x: return 1
-	if x == y: return 0
-
-print(sorted([1, 2, 3, 4, 55, 55, 22], key=functools.cmp_to_key(my_srt)))
-
-arr.sort()
-arr.sort(reverse=True)
-
-from operator import itemgetter, attrgetter
-arr.sort(key=itemgetter(1))
-arr.sort(key=attrgetter('age'))
-
 
 ##################
 ##### DICT #######
@@ -342,33 +377,6 @@ arg, ans = [[1, 2, 3], [4, 5, 6, [7, 8, 9]]], []
 iter_arr = lambda arg: [iter_arr(x) for x in arg] if isinstance(arg, list) else ans.append(arg)
 iter_arr(arg)
 
-
-### binary search
-def bin_search(arr, item):
-
-	l, r = 0, len(arr)-1
-	ans = -1
-
-	# equal to because case mid == l == r
-	while l <= r:
-		mid = (l + r) // 2
-
-		if arr[mid] == item:
-			# return idx
-			return mid
-
-		elif arr[mid] > item:
-			r = mid - 1
-
-		else:
-			# case len(arr) == 2, avoid redundant 0
-			l = mid+1
-
-	return ans
-
-print(bin_search([1,3,4,7,23,25], 4))
-
-
 any([1, 0, 0])		# if any True exists
 all([1, 1, 1])		# if all contents are True
 
@@ -386,6 +394,7 @@ from collections import defaultdict
 x = defaultdict(list)
 
 
+# deque
 from collections import deque
 d = deque()
 d.append(1)
@@ -394,6 +403,7 @@ _ = d.pop()
 _ = d.popleft()
 
 
+# bisect
 import bisect
 arr = [1, 2, 3, 6, 7]
 bisect.insort_left(arr, 5)
@@ -417,7 +427,6 @@ s = "asdf"
 
 import textwrap
 textwrap.wrap("amal is under python practice", 10)
-
 
 #################
 ##### GOTCHA ####
